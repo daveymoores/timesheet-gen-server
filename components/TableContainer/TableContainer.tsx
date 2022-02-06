af9f9ac28d47517505369d8e14e779b22221b641
Davey Moores <daveymoores@gmail.com>
2022-02-06 18:12:18 +0100

    mobile gradients

--- components/TableContainer/TableContainer.tsx
+++ components/TableContainer/TableContainer.tsx
@@ -27,35 +27,45 @@ const TableContainer: React.FC<TableRow> = ({ timesheet }) => {
   const gridClass = className(timesheet.length as 31 | 30 | 29 | 28);
 
   return (
-    <div className="lg:w-full overflow-scroll lg:overflow-auto bg-slate-900 py-4 lg:bg-transparent lg:py-0">
-      <div className="w-[1200px] container max-w-none lg:w-full lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
-        <div className={gridClass("align-middle p-1 grid")}>
-          {timesheet.map(({ hours }, index: number) => {
-            return (
-              <>{hours ? <HourInput hours={hours} index={index} /> : <div />}</>
-            );
-          })}
-        </div>
-        <div className={gridClass("align-middle p-1 bg-black rounded-md grid")}>
-          {timesheet.map(({ weekend }, index: number) => (
-            <div
-              key={index}
-              className={`text-sm lg:text-md text-center font-semibold${
-                weekend ? " text-slate-700" : ""
-              }`}
-            >
-              {index + 1}
-            </div>
-          ))}
-        </div>
-        <div className={gridClass("align-middle p-1 grid")}>
-          {timesheet.map(({ hours }, index: number) => {
-            return (
-              <>
-                {hours ? <HourSymbol hours={hours} index={index} /> : <div />}
-              </>
-            );
-          })}
+    <div className="relative">
+      <div
+        className="lg:w-full overflow-scroll lg:overflow-auto bg-slate-900 py-4 lg:bg-transparent lg:py-0
+    lg:after:hidden after:bg-gradient-to-r after:from-slate-900 after:to-transparent after:bg-transparent after:content-[''] after:absolute after:h-full after:w-4 sm:after:w-8 after:top-0
+    lg:before:hidden before:bg-gradient-to-l before:from-slate-900 before:to-transparent before:bg-transparent before:content-[''] before:absolute before:h-full before:w-4 sm:before:w-8 before:top-0 before:right-0"
+      >
+        <div className="w-[1200px] container max-w-none lg:w-full lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+          <div className={gridClass("align-middle p-1 grid")}>
+            {timesheet.map(({ hours }, index: number) => {
+              return (
+                <>
+                  {hours ? <HourInput hours={hours} index={index} /> : <div />}
+                </>
+              );
+            })}
+          </div>
+          <div
+            className={gridClass("align-middle p-1 bg-black rounded-md grid")}
+          >
+            {timesheet.map(({ weekend }, index: number) => (
+              <div
+                key={index}
+                className={`text-sm lg:text-md text-center font-semibold${
+                  weekend ? " text-slate-700" : ""
+                }`}
+              >
+                {index + 1}
+              </div>
+            ))}
+          </div>
+          <div className={gridClass("align-middle p-1 grid")}>
+            {timesheet.map(({ hours }, index: number) => {
+              return (
+                <>
+                  {hours ? <HourSymbol hours={hours} index={index} /> : <div />}
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
